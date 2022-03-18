490012a488aebab9b34eac455df492831bbe58a2
Mehdi Maujood <maujood@gmail.com>
2022-03-18 16:02:30 -0500

    fixed login crashing bug

--- server/domain/userDomain.js
+++ server/domain/userDomain.js
@@ -3,20 +3,22 @@ const db = require('../db');
 userDomain = {
     //returns array of strings
     createOrGetUserRecord(username, email) {
-        return db.execWithParams('SELECT id, username, email FROM public.users WHERE username = $1', [username])
+        return new Promise(function (resolve, reject) {
+            db.execWithParams('SELECT id, username, email FROM public.users WHERE email = $1', [email])
             .then((result) => {
                 console.log('User found: ' + JSON.stringify(result.rows));
                 if (result.rows.length === 1) {
-                    return result.rows[0];
+                    resolve(result.rows[0]);
                 }
                 else {
-                    //if the user with the given username does not exist, create one
+                    //if the user with the given email does not exist, create one
                     db.execWithParams('INSERT INTO users(username, email) VALUES($1, $2) RETURNING id, username, email', [username, email])
                     .then(() => {
-                        return result.rows[0];
+                        resolve(result.rows[0]);
                     });
                 }
             });
+        });
     }
 }
 
