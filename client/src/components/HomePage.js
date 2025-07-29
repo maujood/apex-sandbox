03ee9473db3e920b91a2eebb957e9e3b21812be9
Mehdi Maujood <maujood@gmail.com>
2025-07-29 16:16:44 -0500

    Go aggressive on CodeSteps.dev placement

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -11,12 +11,12 @@ const HomePage = (props) => {
 
     let homeProblemList = () => {
         return <>
+            <CodeStepsLink />
             <p class="slds-text-heading_medium slds-p-top_medium">Practice makes experts. Take your Apex journey to the next level with a growing collection of Apex practice problems!</p>
             <p class="slds-text-heading_medium slds-p-top_medium">It's easy and free. Log in with a Salesforce Developer Edition (sign up on <a href="https://developer.salesforce.com/signup" target="_blank" rel="noreferrer">developer.salesforce.com/signup</a>) and start knocking out problems!</p>
             <div class="slds-p-vertical_x-large slds-align_absolute-center">
                 <LoginButton bigbutton />
             </div>
-            <CodeStepsLink />
         </>;
     }
     if (user.loggedIn) {
